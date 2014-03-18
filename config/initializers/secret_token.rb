cc009eacf1f3dfa8deaeac36b439efc8a4d8cd82
Simona <simona@crosscamp.us>
2014-03-17 22:22:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kimiglitters::Application.config.secret_key_base = '39f4813af0976f669b238a7c4c831d01d815e783d18c601a8c25fb6c40453b704ce10be95973e1f2bff688f1a6d438e18ccdbf4ac4821e7f16cb8c0c4fd8dbcb'
