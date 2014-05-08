681e5f18af142fdc61403292c36b11cff341f1af
Simona <simona@crosscamp.us>
2014-05-07 22:05:11 -0700

    added authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430032727) do
+ActiveRecord::Schema.define(version: 20140503191259) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
