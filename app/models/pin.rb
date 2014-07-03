2c4754cff81aa11191c5b2713a623a0dee04c118
Simona Marie Asinovski <me@simonamarie.com>
2014-07-03 16:52:34 -0400

    added Amazon S3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, 
+	:styles => { :medium => "300x300>", :thumb => "100x100>" },
+	:storage => :s3,
+	:bucket => 'kimiglitters',
+	:s3_credentials => {:access_key_id => ENV['AKIAJBQMHEK2RSVALDQQ'], :secret_access_key => ENV['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']}
 end
