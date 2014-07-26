cf84270a06f117682a4c05fa4414b4c8e0e229c9
Simona Asinovski <me@simonamarie.com>
2014-07-26 15:58:34 -0700

    changed s3 var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Kimiglitters::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET'],
+    :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['S3_ACCESS_KEY'],
     :secret_access_key => ENV['S3_SECRET_KEY']
     }
