eeb68509f2cdd5be11b5d2099fb9be3c521c0259
Simona Asinovski <me@simonamarie.com>
2014-07-30 19:46:42 -0700

    aws changes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,18 +1,17 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image, :image_remote_url
+	belongs_to :user
+
+	has_attached_file :image, 
+	:styles => { :medium => "300x300>", :thumb => "100x100>" },
+	:storage => :s3,
+	:bucket => 'kimiglittersapp',
+	:s3_credentials => {
+                    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                },
+
+	validates :image, presence: true
+	validates :description, presence: true
 
 
-  validates :description, presence: true
-  validates :user_id, presence: true
-  validates_attachment :image, presence: true,
-  														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-  														 size: { less_than: 5.megabytes }
-
-  belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240>"}
-
-  def image_remote_url=(url_value)
-  	self.image = URI.parse(url_value) unless url_value.blank?
-  	super
-  end
 end
