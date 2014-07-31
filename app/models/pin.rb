88b5fd13d144051d8bbe418d95032d85f0ad49d0
Simona Asinovski <me@simonamarie.com>
2014-07-30 19:38:56 -0700

    changed pin dot rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,14 +1,18 @@
 class Pin < ActiveRecord::Base
-	belongs_to :user
-
-	has_attached_file :image, 
-	:styles => { :medium => "300x300>", :thumb => "100x100>" },
-	:storage => :s3,
-	:bucket => 'kimiglitters',
-	:s3_credentials => {:access_key_id => ENV['AKIAJBQMHEK2RSVALDQQ'], :secret_access_key => ENV['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']}
-
-	validates :image, presence: true
-	validates :description, presence: true
+  attr_accessible :description, :image, :image_remote_url
 
 
+  validates :description, presence: true
+  validates :user_id, presence: true
+  validates_attachment :image, presence: true,
+  														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+  														 size: { less_than: 5.megabytes }
+
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "320x240>"}
+
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  	super
+  end
 end
