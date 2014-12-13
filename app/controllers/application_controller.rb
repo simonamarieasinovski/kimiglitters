c5675485979a90f5af029c491567b4cfb6c65c54
Simona Asinovski <me@simonamarie.com>
2014-12-13 13:35:50 -0800

    Add Amazon S3 for Paperclip uploads to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+	before_filter :set_current_user
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def set_current_user 
+  	@current_user=current_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140511024753) do
+ActiveRecord::Schema.define(version: 20141120025334) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20140511024753) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
